ccad897889eb54291e5ecca0e89b5ea447cdbb6c
xandone <765478955@qq.com>
2017-02-20 16:56:36 +0800

    Modification Snow

--- app/src/main/java/app/xandone/com/edelweiss/MainActivity.java
+++ app/src/main/java/app/xandone/com/edelweiss/MainActivity.java
@@ -4,17 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private SnowflakeView snowflakeView;
-    private SnowflakeView snowflakeView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        snowflakeView = (SnowflakeView) findViewById(R.id.snowflakeView);
-//        snowflakeView1 = (SnowflakeView) findViewById(R.id.snowflakeView1);
-//
-//        snowflakeView.snowAnim2(6000);
-//        snowflakeView1.snowAnim2(1000);
     }
 }
